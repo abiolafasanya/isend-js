363cd166b997762674cc9ef097bfed41b21c2441
fastbeetech <hezekiahfasanya@gmail.com>
2023-05-01 08:46:57 +0100

    update:otp added error message notification

--- src/scenes/auth/Otp/Otp.jsx
+++ src/scenes/auth/Otp/Otp.jsx
@@ -14,31 +14,51 @@ const Otp = () => {
   const navigate = useNavigate();
   const handleLogin = async (e) => {
     e.preventDefault();
-    const email = JSON.parse(localStorage.getItem('auth')).user.email
+    const email = JSON.parse(localStorage.getItem('auth')).user.email;
     const body = {
       otp: e.target.otp.value,
-      email
+      email,
     };
-  //   handleSetAuth({isLoggedIn: true})
-  //   toast.success('Authenticated');
-  //   navigate('/', {replace: true});
-  //  return console.log(body);
 
-    const { data } = await Axios.post('/admin/verify', body);
-    console.log(data);
-    setLoading(true)
-    if (data.success) {
-      toast.success('Authenticated');
-      setLoading(false)
-      handleSetAuth({isLoggedIn: true, user: {email}});
-      setTimeout(() => {
-        navigate('/', {replace: true});
-      }, 1000);
 
+    if (body.otp === '') {
+      toast.error('Please enter a valid otp code');
+      return;
     }
-    if (data.error) {
-      toast.error(data.message);
-      setLoading(false)
+
+    //   handleSetAuth({isLoggedIn: true})
+    //   toast.success('Authenticated');
+    //   navigate('/', {replace: true});
+    //  return console.log(body);
+
+    try {
+      const { data, status } = await Axios.post('/admin/verify', body);
+      console.log(data);
+      setLoading(true);
+      if (data.success || status === 200) {
+        toast.success('Authenticated');
+        setLoading(false);
+        handleSetAuth({ isLoggedIn: true, user: { email } });
+        setTimeout(() => {
+          navigate('/', { replace: true });
+        }, 1000);
+      }
+      if (status === 401 || status === 400 || data.error) {
+        console.log('catch error: ' + data.error)
+        toast.error(data.message);
+        setLoading(false);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        const errorMessage = error.response.data || error.response.data.message;
+        console.log('Error message:', errorMessage);
+        setLoading(false);
+        toast.error(errorMessage);
+        return;
+      }
+      setLoading(false);
+      toast.error('Login failed');
+      console.error('Error occurred during login:', error);
     }
   };
   return (
@@ -51,10 +71,10 @@ const Otp = () => {
             type="text"
             name="otp"
             id="otp"
-            placeholder='Enter your Otp Code'
+            placeholder="Enter your Otp Code"
             className={styles.form_input}
           />
-           <LoadingButton
+          <LoadingButton
             loading={loading}
             loadingPosition="end"
             variant="contained"
@@ -73,8 +93,7 @@ const Otp = () => {
           </button> */}
         </form>
       </div>
-      <div className={styles.bottom}>
-      </div>
+      <div className={styles.bottom}></div>
       <ToastContainer />
     </div>
   );
